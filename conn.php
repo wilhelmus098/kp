84c834539d581e648104cf5cfda6e6e2c05fea3a
Wilhelm Mozes <wilhelmmozes@gmail.com>
2019-04-29 10:36:15 +0700

    update database location

--- conn.php
+++ conn.php
@@ -1,14 +1,18 @@
 <?php
 
-$srvName = "wilhelmmozes.site"; //SERVER ADDRESS OR IP SERVER
-$srvUser = "wilhelmus"; // USER ID TO DATABASE
-$srvPWD = "12345678"; //PWD TO ACCESS DATABASE
-$dbName = "kp"; //DATABASE NAME
+$srvName = "sql172.main-hosting.eu"; //SERVER ADDRESS OR IP SERVER
+$srvUser = "u947864388_will"; // USER ID TO DATABASE
+$srvPWD = "087886002060"; //PWD TO ACCESS DATABASE
+$dbName = "u947864388_kp"; //DATABASE NAME
 
 $mysqli = mysqli_connect($srvName,$srvUser,$srvPWD,$dbName);
 
   if (mysqli_connect_errno())
   {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  else
+  {
+    echo "sukses";
   }
  ?>
